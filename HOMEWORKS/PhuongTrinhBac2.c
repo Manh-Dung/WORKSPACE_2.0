65f8979072873675a58694429664f0d8d67f04b7
Manh Dung <manhdung5a@gmail.com>
2021-11-01 14:16:47 +0700

    Nhập xuất file với phương trình bậc 2

--- HOMEWORKS/PhuongTrinhBac2.c
+++ HOMEWORKS/PhuongTrinhBac2.c
@@ -2,32 +2,44 @@
 #include<conio.h>
 #include<stdio.h>
 #include<string.h>
-main()
+#include<stdlib.h>
+
+int main()
 {
+    FILE *fptr;
     float a,b,c,d;
+
+    if ((fptr = fopen ("input.txt" , "r")) == NULL){
+        printf("Error! opening file");
+       exit(1);
+    }
+
     printf("Nhap vao 3 so a, b, c: ");
-    scanf("%f%f%f",&a,&b,&c);
+    fscanf(fptr, "%f %f %f",&a,&b,&c);
+
+    fptr = fopen("output.txt","w");
+
     if(a==0)
     {
         if(b==0)
             {
                 if(c==0)
-                    printf("Phuong trinh co vo so nghiem!");
+                    fprintf(fptr, "Phuong trinh co vo so nghiem!\n");
                 else
-                    printf("Phuong trinh vo nghiem!");
+                    fprintf(fptr, "Phuong trinh vo nghiem!\n");
             }
         else
-            printf("Phuong trinh co nghiem duy nhat la: %f",-c/b);
+            fprintf(fptr, "Phuong trinh co nghiem duy nhat la: %f\n",-c/b);
     }
     else
     {
         d=b*b-4*a*c;
         if (d<0)
-            printf("Phuong trinh vo nghiem!!!");
+            fprintf(fptr, "Phuong trinh vo nghiem!!!\n");
         else if (d==0)
-            printf("Phuong trinh co nghiem kep la: %f",-b/(2*a));
+            fprintf(fptr, "Phuong trinh co nghiem kep la: %f\n",-b/(2*a));
         else
-            printf("Phuong trinh co 2 nghiem phan biet la: %f,%f",(-b+sqrt(d))/(2*a),(-b-sqrt(d))/(2*a));    
+            fprintf(fptr, "Phuong trinh co 2 nghiem phan biet la: %f,%f\n",(-b+sqrt(d))/(2*a),(-b-sqrt(d))/(2*a));    
     }    
   
 }
