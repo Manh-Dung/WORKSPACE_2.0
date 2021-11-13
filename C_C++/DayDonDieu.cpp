c1b3929955d0c8cffe93772f6fe83c092e9b13c1
Manh Dung <manhdung5a@gmail.com>
2021-11-13 16:52:56 +0700

    chua co time sr

---
+++ C_C++/DayDonDieu.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+using namespace std;
+
+int main () {
+    int n;
+    cin >> n;
+    int a[100];
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    bool kt = true;
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = i + 1; j < i + 2; j++) {
+            if (a[i] >= a[j]) {
+                
+            }
+        }
+    }
+
+}
+
+
